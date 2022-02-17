63b8a91053d97ff0d5e9c1d7aa7ad1ee3f8db205
nhoteling <nhoteling@gmail.com>
2022-02-17 15:21:29 -0500

    Add Pages site

---
+++ docs/_site.R
@@ -0,0 +1,17 @@
+## packages I want loaded for all pages of my site
+suppressPackageStartupMessages({
+  library(rvest)
+  library(dplyr)
+  library(tidyr)
+  library(stringr)
+  library(stringdist)
+  library(osmdata)
+  library(ggplot2)
+  library(lubridate)
+  library(forecast)
+  library(kableExtra)
+  library(textreuse)
+  library(lubridate)
+  source("../R/utils.R")
+})
+
